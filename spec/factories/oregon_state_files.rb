ead49e8b098a3814bad19ebd72384d8077554166
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-04 00:40:13 -0700

    add spec for oregon state files

---
+++ spec/factories/oregon_state_files.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :oregon_state_file do
+    data_type 1
+    query ""
+    downloaded_file ""
+    downloaded_at "2014-10-03 23:07:54"
+  end
+end
