918f3e0d9448bc5b54efcd62219f644ae9312b9b
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-04 16:38:12 -0700

    add candidate_transactions table

---
+++ db/migrate/20141004074114_create_candidate_transactions.rb
@@ -0,0 +1,56 @@
+class CreateCandidateTransactions < ActiveRecord::Migration
+  def change
+    create_table :candidate_transactions do |t|
+      t.references :oregon_state_file
+      t.integer :source_id
+      t.string :original_id
+      t.date :transaction_date
+      t.string :transaction_status
+      t.string :filer
+      t.string :contributor_payee
+      t.string :sub_type
+      t.decimal :amount
+      t.decimal :aggregate_amount
+      t.integer :contributor_payee_committee_id
+      t.integer :filer_id
+      t.string :attest_by_name
+      t.date :attest_date
+      t.string :review_by_name
+      t.date :review_date
+      t.date :due_date
+      t.date :occptn_ltr_date
+      t.text :payment_schedule_txt
+      t.string :purpose_description
+      t.decimal :interest_rate
+      t.string :check_number
+      t.string :filed_by_name 
+      t.string :tran_stsfd_indicator
+      t.date :filed_date
+      t.string :addr_book_agent_name
+      t.string :book_type
+      t.string :title
+      t.string :occupation
+      t.string :employer_name
+      t.string :employer_city
+      t.string :employer_state
+      t.string :employer_indicator
+      t.string :self_employed_indicator
+      t.string :address_line1
+      t.string :address_line2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :zip_plus_four
+      t.string :county
+      t.string :purpose_codes
+      t.date :exp_date
+
+      t.timestamps
+    end
+
+    add_index :candidate_transactions, :source_id
+    add_index :candidate_transactions, :oregon_state_file_id
+    add_index :candidate_transactions, :transaction_date
+    add_index :candidate_transactions, :filed_date
+  end
+end
