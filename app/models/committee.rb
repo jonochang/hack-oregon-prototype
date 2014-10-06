e515cb1f50ae7d2030dcd2b43e565d64f06cb9fd
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-06 09:50:38 -0700

    committee should have many campaign finance transactions

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -2,5 +2,7 @@ class Committee < ActiveRecord::Base
   belongs_to :oregon_state_file
   belongs_to :filing_date_entity, class_name: 'Analytics::DateEntity'
 
+  has_many :campaign_finance_transactions
+
   validates :source_id, presence: true, numericality: true
 end
