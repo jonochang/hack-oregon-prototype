27d8c02e88c46568e958e552db36f1cf118ddfc1
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-06 09:49:27 -0700

    add relationships for campaign finance transactions

--- app/models/campaign_finance_transaction.rb
+++ app/models/campaign_finance_transaction.rb
@@ -1,5 +1,9 @@
 class CampaignFinanceTransaction < ActiveRecord::Base
   belongs_to :oregon_state_file
+  belongs_to :transaction_type
+  belongs_to :committee
+  belongs_to :transaction_date_entity, class_name: 'Analytics::DateEntity'
+  belongs_to :filed_date_entity, class_name: 'Analytics::DateEntity'
 
   validates :source_id, presence: true, numericality: true
 end
