9562e8caf697c155a01a31efd07b005a56c4b436
Jonathan Chang <jonathan.chang@silverpond.com.au>
2014-10-08 22:29:39 -0700

    update paperclip due to issues with csv file generated

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,13 @@ Paperclip.options[:content_type_mappings] = {
   :xls => "CDF V2 Document, No summary info"
 }
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+
+#tried to manually handle different content types that got created, and finally got defeated by a csv registering as octet stream / binary
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
